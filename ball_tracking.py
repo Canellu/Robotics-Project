8501c85945c1ce1e1fab64474df57016ad628e09
Anton Vo <S300938@oslomet.no>
2020-11-12 21:23:51 +0100

    Adding first file...

---
+++ ball_tracking.py
@@ -0,0 +1,17 @@
+# import the necessary packages
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+	help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+	help="max buffer size")
+args = vars(ap.parse_args())
+
+
