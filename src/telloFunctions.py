e8c94f32b631f7ca2e18772cce07514f08f19ca6
einar tomter <einatomter@gmail.com>
2020-11-22 14:16:43 +0100

    face cascade add

--- src/telloFunctions.py
+++ src/telloFunctions.py
@@ -25,3 +25,11 @@ def telloGetFrame(drone, frameWidth=360, frameHeight=240):
     img = cv2.resize(telloFrame,(frameWidth,frameHeight))
     
     return img
+
+def findFace(img):
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(imgGray, 1.2,4)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
