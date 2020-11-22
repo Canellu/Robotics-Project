2ac2b168d82f2c39731d0e348e172782863d9a43
einar tomter <einatomter@gmail.com>
2020-11-22 17:00:35 +0100

    tracking added

--- src/telloFunctions.py
+++ src/telloFunctions.py
@@ -1,5 +1,6 @@
 from djitellopy import Tello
 from cv2 import cv2
+import numpy as np
 
 def initializeTello():
 
@@ -27,9 +28,58 @@ def telloGetFrame(drone, frameWidth=360, frameHeight=240):
     return img
 
 def findFace(img):
+
+    # prediction
     faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(imgGray, 1.2,4)
 
+    myFaceListC = []
+    myFaceListArea = []
+
     for(x,y,w,h) in faces:
+
+        # drawing face boundary
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+
+        # finding face center coordinate
+        cx = x + w//2
+        cy = y + h//2
+        area = w*h
+        myFaceListArea.append(area)
+        myFaceListC.append([cx,cy])
+
+    if len(myFaceListArea) != 0:
+
+        # finding closest face (biggest area)
+        i = myFaceListArea.index(max(myFaceListArea))
+        return img, [myFaceListC[i], myFaceListArea[i]]
+    
+    else:
+        return img,[[0,0],0]
+
+def trackFace(drone, info, w, pid, pError):
+
+    # PID
+    error = (info[0][0] - w//2)
+    speed = pid[0]*error + pid[1]*(error-pError)
+    speed = int(np.clip(speed,-100, 100))
+    print(speed)
+
+    if info[0][0] != 0:
+        drone.yaw_velocity = speed
+    else:
+        drone.for_back_velocity = 0
+        drone.left_right_velocity = 0
+        drone.up_down_velocity = 0
+        drone.yaw_velocity = 0
+        drone.speed = 0
+        error = 0
+
+    if drone.send_rc_control:
+        drone.send_rc_control(drone.left_right_velocity,
+                              drone.for_back_velocity,
+                              drone.up_down_velocity,
+                              drone.yaw_velocity)
+
+    return error
--- src/telloMain.py
+++ src/telloMain.py
@@ -1,16 +1,43 @@
 from telloFunctions import *
 
 
-frameWidth, frameHeight = 640, 360
+frameWidth, frameHeight = 360, 240
+pid = [0.4, 0.4, 0]
+pError = 0
+startCounter = 0 # fly = 0, no fly = 1
+quit = 0
+# manualControl = True
 
 drone = initializeTello()
 
-while True:
+# Flight
+if startCounter == 0:
+    drone.takeoff()
+    startCounter = 1
+
+while quit != 1:
     
     # Step 1
     img = telloGetFrame(drone, frameWidth, frameHeight)
+
+    # Step 2
+    img, info = findFace(img)
+
+    # Step 3
+    pError = trackFace(drone, info, frameWidth, pid, pError)
+
+    # switch manual/tracking WIP
+
+    # printing y center coordinate
+    # print(info[0][1])
+
     cv2.imshow('The Window', img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         drone.land()
+        quit = 1
         break
+    # elif cv2.waitKey(1) & 0xFF == ord('m'):
+    #     manualControl = !manualControl
+
+drone.end()
