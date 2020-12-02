c5c41bdf71f6767b025cecf897e308c485665bc4
einatomter <55589561+einatomter@users.noreply.github.com>
2020-12-02 11:41:54 +0100

    info changed from area to w,h

--- src/telloFunctions.py
+++ src/telloFunctions.py
@@ -22,17 +22,17 @@ def initializeTello():
         drone.yaw_velocity = 0
         drone.speed = 0
 
-        
+
         drone.streamoff()
         drone.streamon()
         print(f"BATTERY: {drone.get_battery()}")
         print("---- Connecting to drone Succeeded ----\n")
-        
+
     else:
         print("\n---- Connecting to drone Failed ----\n")
 
     return connection, drone
-    
+
 
 
 def telloGetFrame(drone, frameWidth=360, frameHeight=240):
@@ -40,7 +40,7 @@ def telloGetFrame(drone, frameWidth=360, frameHeight=240):
     telloFrame = drone.get_frame_read()
     telloFrame = telloFrame.frame
     img = cv2.resize(telloFrame,(frameWidth,frameHeight))
-    
+
     return img
 
 
@@ -64,30 +64,30 @@ def findFace(img):
         cy = y + h//2
         area = w*h
         myFaceListArea.append(area)
-        myFaceListC.append([cx,cy])
+        myFaceListC.append([cx, cy, w, h])
 
     if len(myFaceListArea) != 0:
 
         # finding closest face (biggest area)
         i = myFaceListArea.index(max(myFaceListArea))
-        return img, [myFaceListC[i], myFaceListArea[i]]
-    
+        return img, myFaceListC[i]
     else:
-        return img,[[0,0],0]
+
+        return img,[0,0,0,0]
 
 
 def findFaceYolo(outputs, img, classNames):
 
 
     # Neural Network Params
-    confThreshold = 0.9 # Lower value, more boxes (but worse confidence per box)
-    nmsThreshold = 0.3 # Lower value, less overlaps
+    confThreshold = 0.1 # Lower value, more boxes (but worse confidence per box)
+    nmsThreshold = 0.1 # Lower value, less overlaps
 
     hT, wT, _ = img.shape
     bbox = [] 
     classIndices = []
     confs = []
-    cupArea = []
+    returnArea = []
 
     for output in outputs: # Go through each output layer (3 layers)
         for det in output: # Go through each detection in layers (rows per layer: 300 first layer, 1200 second layer, 4800 third layer)
@@ -104,47 +104,60 @@ def findFaceYolo(outputs, img, classNames):
 
     # Returns indices of boxes to keep when multiple box on same object. Finds box with highest probability, suppress rest.
     indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
-    
+
     for i in indices:
         i = i[0] # Flatten indices, comes as a value in list. [val] --> val
         box = bbox[i] # Get a box
         x, y, w, h = box[0], box[1], box[2], box[3] # Extract x, y, width, height
         area = w * h
-        
-        if(classNames[classIndices[i]] == 'cup'):
+
+        if(classNames[classIndices[i]] == 'Human face'):
             cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,255), 2) # Draw bounding box
             cv2.putText(img, f'{classNames[classIndices[i]].upper()} {int(confs[i]*100)}%', 
                                 (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,255), 2) #Write class name and % on bounding box
-            cupArea.append(area)
-            
-        
-    if len(cupArea) != 0:
+            returnArea.append(area)
 
+
+    if len(returnArea) != 0:
         # finding closest face (biggest area)
-        i = cupArea.index(max(cupArea))
-        cx = bbox[i][0] + bbox[i][2]/2
-        cy = bbox[i][1] + bbox[i][3]/2
-        return img, ([[cx,cy], cupArea[i]])
-       
+        i = returnArea.index(max(returnArea))
+        bbox[i][0] = bbox[i][0] + bb[i][2]
+        bbox[i][1] = bbox[i][1] + bb[i][3]
+
+        return img, (bbox[i])   
     else:
-        return img, ([[0,0],0])
+        return img, ([0,0,0,0])
 
 def trackFace(drone, info, pInfo, w, h, pidYaw, pidX, pidZ, pError):
 
-    error = [0,0,0] # yaw, height, distance
-    speed = [0,0,0]
-    x = 0
+    error = [0,0,0] # yaw, height, distance (pixels)
+    speed = [0,0,0] # yaw, height, distance (cm/s)
+    wait = 0 # s
+
+    # current info
+    cx = info[0]
+    cy = info[1]
+    bw = info[2]
+    bh = info[3]
+
+    # previous info
+    pcx = pInfo[0]
+    pcy = pInfo[1]
+    pbw = pInfo[2]
+    pbh = pInfo[3]
 
     # editable variables
     percentArea = 1/25
+    edgeDetecion = False
     sideDetecThreshold = 2
+
 
     # calculations
 
     area = w * h * percentArea
-    error[0] = (info[0][0] - w//2) #//w * 120
-    error[1] = (info[0][1] - h//2)
-    error[2] = (info[1] - area)/25
+    error[0] = (cx - w//2) #//w * 120
+    error[1] = (cy - h//2)
+    error[2] = ((bw * bh) - area)/25
 
     # PID
 
@@ -162,23 +175,23 @@ def trackFace(drone, info, pInfo, w, h, pidYaw, pidX, pidZ, pError):
     # print(f"error: {error[0]}\t speed: {speed[0]}") # yaw
     # print(f"error: {error[1]}\t speed: {speed[1]}") # height
     # print(f"error: {error[2]}\t speed: {speed[2]}") # distance
-    # print(f"center x: {info[0][0]} center y: {info[0][1]}") # center coordinate
+    # print(f"center x: {cx} center y: {cy}") # center coordinate
     # print(f"current info: {info}\t previous info: {pInfo}")
 
 
-    if info[0][0] != 0:
+    if cx != 0:
         drone.yaw_velocity = speed[0]
     else:
         # testing if object went out of frame left/right
-        if pInfo[0][0] != 0:
-            if pInfo[0][0] < (w*sideDetecThreshold)//10: #left
+        if pcx != 0 and edgeDetecion == True:
+            if pcx < (w*sideDetecThreshold)//10: #left
                drone.yaw_velocity = -100
-               x = 0.15
+               wait = 0.15
             #    drone.rotate_counter_clockwise(360)
                print("chasing left")
-            elif pInfo[0][0] > (w*(10-sideDetecThreshold))//10: #right
+            elif pcx > (w*(10-sideDetecThreshold))//10: #right
                drone.yaw_velocity = 100
-               x = 0.15
+               wait = 0.15
             #    drone.rotate_clockwise(360)
                print("chasing right")
         # if nothing is being tracked
@@ -187,25 +200,25 @@ def trackFace(drone, info, pInfo, w, h, pidYaw, pidX, pidZ, pError):
             drone.yaw_velocity = 0
             error[0] = 0
 
-    if info[0][1] != 0:
+    if cy != 0:
         drone.up_down_velocity = speed[1]
     else:
         # testing if object went out of frame up/down
-        if pInfo[0][1] != 0:
-            if pInfo[0][1] < (h*sideDetecThreshold)//10: #up
+        if pcy != 0 and edgeDetecion == True:
+            if pcy < (h*sideDetecThreshold)//10: #up
                drone.up_down_velocity = 100
-               x = 0.15
+               wait = 0.15
                print("chasing up")
-            elif pInfo[0][1] > (h*(10-sideDetecThreshold))//10: #down
+            elif pcy > (h*(10-sideDetecThreshold))//10: #down
                drone.up_down_velocity = -100
-               x = 0.15
+               wait = 0.15
                print("chasing down")
         # if nothing is being tracked
         else:
             drone.up_down_velocity = 0
             error[1] = 0
 
-    if info[1] != 0:
+    if (bw * bh) != 0:
         drone.for_back_velocity = speed[2]
     else:
         drone.for_back_velocity = 0
@@ -216,15 +229,15 @@ def trackFace(drone, info, pInfo, w, h, pidYaw, pidX, pidZ, pError):
                               drone.for_back_velocity,
                               drone.up_down_velocity,
                               drone.yaw_velocity)
-    
-    time.sleep(x)
+
+    time.sleep(wait)
 
     pInfo = info
 
     return pInfo, error
 
 def drawOSD(drone, frame, frameWidth, frameHeight):
-   
+
     # Data retrieved from drone
     data = [0] * 10
     data[0] = str(drone.send_command_with_return('speed?')) # Speed in cm/s 1-100
--- src/telloMain.py
+++ src/telloMain.py
@@ -3,24 +3,20 @@ import time
 import cv2
 import numpy as np
 
-whT = 320 # A parameter for image to blob conversion
+whT = 192 # A parameter for image to blob conversion
 
 # Import class names to list from coco.names
-classesFile = "../YOLOv3/coco.names"
+classesFile = "../YOLOv3/obj.names"
 classNames = []
 with open(classesFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
 # Set up model and network
-modelConfig = "../YOLOv3/yolov3.cfg"
-modelWeights = "../YOLOv3/yolov3.weights" 
+modelConfig = "../YOLOv3/yolov3_custom.cfg"
+modelWeights = "../YOLOv3/yolov3_custom.weights" 
 net = cv2.dnn.readNetFromDarknet(modelConfig, modelWeights)
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-
-
-
-
 
 frameWidth, frameHeight = 360, 240
 # automatic scaling
@@ -30,7 +26,7 @@ frameWidth, frameHeight = 360, 240
 pidYaw = [0.5, 0.5, 0]
 pidZ = [0.7, 0.6, 0]
 pidX = [0.5, 0.2, 0]
-pInfo = [[0, 0], 0] # [x,y], area
+pInfo = [0, 0, 0, 0] # x, y, width, height
 pError = [0, 0, 0] # yaw, height, distance
 
 
@@ -67,8 +63,8 @@ while connection:
                                       # box is, rest is probability per classes) )
 
     # Step 2 
-    # img, info = findFace(img)
-    img, info = findFaceYolo(outputs, img, classNames)
+    img, info = findFace(img)
+    # img, info = findFaceYolo(outputs, img, classNames)
 
     # Step 3 Control drone movement to track object
     pInfo, pError = trackFace(drone, info, pInfo, frameWidth, frameHeight, pidYaw, pidX, pidZ, pError)
@@ -85,10 +81,11 @@ while connection:
     cv2.imshow('Display', img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        drone.land()
         drone.end()
         safeQuit = True
         break
- 
+
 
 if not safeQuit:
     print(drone.get_battery()) # temp to check battery
